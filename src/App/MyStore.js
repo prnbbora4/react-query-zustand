d76da7f2268d64807d86b834c8c7290f75eaff70
Pranab Bora <prnbbora4@gmail.com>
2022-07-18 15:50:29 +0530

    fetch data using react query set data to zustand store

--- src/App/MyStore.js
+++ src/App/MyStore.js
@@ -1,25 +1,14 @@
-import React from 'react';
 import create from 'zustand';
-import { useQuery } from 'react-query';
 import { devtools, persist } from 'zustand/middleware'
 
 
-const fetchData2 = async () => {
-    const response = await fetch('https://swapi.co/api/people');
-    const data = await response.json();
-    return data.results;
-}
-
-const ApiCall = (set) => {
-    const { data, status } = useQuery('people', fetchData2);
-    return set({ dataHouse: data });
-
-}
-
-
 const myStore = (set) => ({
-    dataHouse: [],
-    fetchdata: ApiCall
+    dataHouse: {},
+    addData: (data) => {
+        set((state) => ({
+            dataHouse: { data, ...state },
+        }))
+    },
 })
 
 const useMyStore = create(
--- src/components/MyData.js
+++ src/components/MyData.js
@@ -1,29 +1,34 @@
 import React from 'react';
+import { useQuery } from 'react-query';
 import useMyStore from '../App/MyStore';
 
 const People = () => {
+    const myfunction = async () => {
+        const response = await fetch('http://swapi.dev/api/people/')
+        const savedData = await response.json()
+        return savedData.results
+    }
 
-    const { dataHouse, } = useMyStore(
-        (state) => ({
-            dataHouse: state.dataHouse,
-        })
-    )
+    const dataHouse = useMyStore((state) => state.dataHouse)
+    const addData = useMyStore((state) => state.addData)
 
-    console.log("dataHouse", dataHouse);
+
+    const { data, status } = useQuery('people', myfunction);
+
+    console.log("data", data);
+
+    if (status === 'loading') {
+        return <div>Loading...</div>
+    }
+    if (status === 'error') {
+        return <div>Error!</div>
+    }
+
 
     return (
         <div>
             <h2>People</h2>
-            {
-                dataHouse.results.map((person, index) => {
-                    return (
-                        <div key={index}>
-                            <p>{person.name}</p>
-                        </div>
-                    )
-                })
-            }
-
+            <button onClick={() => { addData(data) }}>set</button>
         </div>
     );
 }
